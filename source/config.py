d2511bb6a45f48122c671d9972dd3e0ed81074cf
RomeoCoding <165141163+RomeoCoding@users.noreply.github.com>
2025-03-01 12:35:40 +0200

    fixed paths

--- source/config.py
+++ source/config.py
@@ -18,8 +18,6 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 print(openai.api_key)
 APP_PATHS = {
     "chrome": "C:\Program Files\Google\Chrome\Application\chrome.exe",
-    "notepad": "C:\\Windows\\system32\\notepad.exe",
-    "calculator": "C:\\Windows\\System32\\calc.exe"
 }
 
 DEBUG = True
