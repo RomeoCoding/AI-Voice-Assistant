5a816d5ec31b4944869e01e060f6688bc2247e7e
RomeoCoding <165141163+RomeoCoding@users.noreply.github.com>
2025-03-01 13:06:55 +0200

    Acess Key

--- source/src/wake_word_detector.py
+++ source/src/wake_word_detector.py
@@ -1,12 +1,15 @@
 import pvporcupine
 import pyaudio
 import config
-
+import os
 def wake_word_listener(callback):
     try:
         # Set the correct wake word from config, like "picovoice" or any valid one
-        porcupine = pvporcupine.create(keyword_paths=[pvporcupine.KEYWORD_PATHS[config.WAKE_WORD]])
-
+        access_key = os.getenv("PICOVOICE_ACCESS_KEY")
+        porcupine = pvporcupine.create(
+            keyword_paths=[pvporcupine.KEYWORD_PATHS[config.WAKE_WORD]],
+            access_key=access_key  # Pass the access key here
+        )
         # Start audio stream
         pa = pyaudio.PyAudio()
         stream = pa.open(rate=porcupine.sample_rate,
